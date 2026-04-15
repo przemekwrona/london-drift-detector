c79a0f5a4e01286b51ce17b5393639515b5ae97a
przemekwrona <przemekwrona25@gmail.com>
2026-04-15 23:44:55 +0200

    feat: define author

--- test/london_drift_detector/merger/test_merge_csv_to_python.py
+++ test/london_drift_detector/merger/test_merge_csv_to_python.py
@@ -1,2 +1,12 @@
+from pathlib import Path
+from london_drift_detector.merger import file_merger as prqt
+
+
 def test_read_csv_and_save_as_parquest_in_folder_2026_04_12(tmp_path):
-    print("")
+    # given
+    directory = Path(__file__).resolve().parent.parent / "resources" / "2024-03-10"
+
+    # when
+    prqt.merge_csvs_to_parquet(directory=directory)
+
+    # then
