a9668b1886f1cbcf2096178195f7988776e548a2
przemekwrona <przemekwrona25@gmail.com>
2026-04-16 00:31:46 +0200

    feat: covert to date time

--- test/london_drift_detector/merger/test_merge_csv_to_python.py
+++ test/london_drift_detector/merger/test_merge_csv_to_python.py
@@ -5,8 +5,10 @@ from london_drift_detector.merger import file_merger as prqt
 def test_read_csv_and_save_as_parquest_in_folder_2026_04_12(tmp_path):
     # given
     directory = Path(__file__).resolve().parent.parent / "resources" / "2024-03-10"
+    # output_path = Path(__file__).resolve().parent.parent / "resources" / "2024-03-10" / "2024_03_10.parquet"
+    output_path = tmp_path / "2024_03_10.parquet"
 
     # when
-    prqt.merge_csvs_to_parquet(directory=directory)
+    prqt.merge_csvs_to_parquet(directory=directory, output_path=output_path)
 
     # then
