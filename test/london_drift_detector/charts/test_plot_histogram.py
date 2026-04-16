42ce8f3fbe8c2f3318795b34a3b7f2cf36722827
przemekwrona <przemekwrona25@gmail.com>
2026-04-16 19:26:30 +0200

    feat: write test

--- test/london_drift_detector/charts/test_plot_histogram.py
+++ test/london_drift_detector/charts/test_plot_histogram.py
@@ -5,12 +5,11 @@ import pandas as pd
 
 def test_load_parquet_and_plot_histogram(tmp_path):
     # given
-    parquet_data = Path(__file__).resolve().parent.parent / "results" / "2024-03-10.parquet"
-    # "/Users/wronap/workspace/london-drift-detector/test/london_drift_detector/results/2024-03-10.parquet"
+    parquet_path = Path(__file__).resolve().parent.parent / "results" / "2024-03-10.parquet"
 
     # when
-    data = hist.number_of_active_vehicles(parquet_data)
+    data = hist.number_of_active_vehicles(parquet_path)
 
     # then
-    assert pd.Timestamp('2024-03-10 15:45:00') in data.index
-    assert data.loc[pd.Timestamp('2024-03-10 15:45:00')] == 66
+    assert pd.Timestamp('2024-03-10 15:40:00') in data.index
+    assert data.loc[pd.Timestamp('2024-03-10 15:40:00')] == 65
